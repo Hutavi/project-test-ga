509642c1105a979912f786451214f929c357263e
Vinh Huỳnh Tấn <vinhhuynhtan@VN1N07HO1CD1031.local>
2024-12-12 09:33:48 +0700

    update event

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,34 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import ReactGA from "react-ga4";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const handleLinkClick = (label: string) => {
+    ReactGA.event({
+      category: "Navigation",
+      action: "button_click",
+      label: label,
+    });
+  };
+
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a
+          href="https://vite.dev"
+          target="_blank"
+          onClick={() => handleLinkClick("Vite")}
+        >
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a
+          href="https://react.dev"
+          target="_blank"
+          onClick={() => handleLinkClick("React")}
+        >
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
