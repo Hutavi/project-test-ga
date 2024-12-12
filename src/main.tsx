73778658bff466bb24847c516907741a70ae1edb
Vinh Huỳnh Tấn <vinhhuynhtan@VN1N07HO1CD1031.local>
2024-12-12 08:55:29 +0700

    update ga

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode, useEffect } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { initializeAnalytics, logPageView } from "./utils/analytics.ts";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const measurementId = "G-GHF8FTFYYC";
+initializeAnalytics(measurementId);
+
+const Root = () => {
+  useEffect(() => {
+    logPageView();
+  }, []);
+
+  return (
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+};
+
+createRoot(document.getElementById("root")!).render(<Root />);
